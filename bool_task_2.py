ce8771a48eb858b30df6faacd99aab178773c374
Vleda90 <139127463+Vleda90@users.noreply.github.com>
2025-03-19 20:35:36 +0100

    Add files via upload
    Write a script that evaluates the result of the offensive game based on basic parameters and another result: “our team will enter the offensive stage of the championship by defeating the “Metelik” team, not the “Murakha” team, but the “Mukha” team either defeating the “Metelika”, or plays in a draw with the “Murakha” team.”

---
+++ bool_task_2.py
@@ -0,0 +1,5 @@
+win_butterfly = True
+lose_bug = False
+fly_lose_butterfly = False
+fly_draw_bug = True
+print(f'We are going to the next stage of the championship - {win_butterfly and not lose_bug and (fly_lose_butterfly or fly_draw_bug)}')
